0da12d85051024786cc62b2e3ec331db1c2d81c8
NikolayVitanov <shadowthe673@gmail.com>
2022-12-20 20:36:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace from_Hex_to_Dec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която въвежда стойност в
+            //deseti4en формат(0x##) и я
+            //преобразува в shestnaisetichen формат, след което
+            //извежда стойността.
+            int number = int.Parse(Console.ReadLine());
+            int leftOver = 0;
+            while (number > 0)
+            {
+                leftOver = number % 2;
+                Console.Write(leftOver);
+                number = number / 2;
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
